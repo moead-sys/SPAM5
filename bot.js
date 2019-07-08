7b7fdaa01df18acca27cc8a8b95bc8c1b664f934
moead-sys <52639378+moead-sys@users.noreply.github.com>
2019-07-08 09:01:43 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("597573685892612128")
+setInterval(function() {
+channel.send(`potato potato i love potatobot spam 2
+KIKI DO YOU LOVE ARE YOU RIDING`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
